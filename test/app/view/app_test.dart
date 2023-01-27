408962f017ad1df772064828fd37b2a68c5fc71a
4xMafole <4cruxt.unknown@gmail.com>
2023-01-27 19:29:33 +0300

    fix: info

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -3,9 +3,9 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('Testing nothing', () {
     test(
-      "Testing unknown",
+      'Testing unknown',
       () {
-        var total = 1 + 1;
+        const total = 1 + 1;
         expect(total, 2);
       },
     );
