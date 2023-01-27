3c3a86eb5b6d8f7b9e1565800d28557cefae8f36
4xMafole <4cruxt.unknown@gmail.com>
2023-01-27 19:25:15 +0300

    feat: added a simple test.

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,1 +1,13 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Testing nothing', () {
+    test(
+      "Testing unknown",
+      () {
+        var total = 1 + 1;
+        expect(total, 2);
+      },
+    );
+  });
+}
